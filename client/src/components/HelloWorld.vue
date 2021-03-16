1496ac2ceaaa76cde515e0c3a98311d32c305612
vexleet <vex_malamov@abv.bg>
2021-03-16 20:18:23 +0200

    add vue 3 for client side

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="increment">
+    Count is: {{ count }}, double is {{ double }}, click to increment.
+  </button>
+</template>
+
+<script>
+import { ref, computed, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+    const double = computed(() => count.value * 2);
+    console.log(count);
+    function increment() {
+      count.value++;
+    }
+
+    onMounted(() => console.log('component mounted!'));
+
+    return {
+      count,
+      double,
+      increment,
+    };
+  },
+};
+</script>
